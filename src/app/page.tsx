9a5c28fd6bb5f1e8a0e89393fe39413407baddcb
Christopher Villaran <cv.career.github.v0.pgs9v@simplelogin.com>
2025-04-20 16:26:51 +0800

    master: added SASS, extracted DeployBtn into its own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { DeployBtn } from "@/components";
 
 export default function Home() {
   return (
@@ -21,21 +22,7 @@ export default function Home() {
         </ol>
 
         <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
+          <DeployBtn />
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { DeployBtn } from "./DeployBtn/DeployBtn.component";
